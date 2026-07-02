c70fc8e746cd17bc76cad7bf96457d886bca8156
LUIS ENRIKE MAIA DA SILVA <luis-enrike_silva@estudante.sesisenai.org.br>
2026-07-02 20:01:48 -0300

    Atividade modulo

---
+++ Modulo - Node/NIVEL - 1/Ex - 1/calculadora.js
@@ -0,0 +1,6 @@
+const calcular = require('./operacoes');
+console.log(calcular.somar(5, 3));
+console.log(calcular.subtrair(10, 4)); 
+console.log(calcular.multiplicar(2, 3));
+console.log(calcular.divisao(10, 2));
+
