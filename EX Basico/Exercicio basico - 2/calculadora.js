57f270a822ebb84f217560d94f5a458e8f6bff3e
LUIS ENRIKE MAIA DA SILVA <luis-enrike_silva@estudante.sesisenai.org.br>
2026-07-01 21:25:44 -0300

    Atividades Node

---
+++ EX Basico/Exercicio basico - 2/calculadora.js
@@ -0,0 +1,16 @@
+let numero1 = 2;
+let numero2 = 10;
+
+console.log("Numero 1 = ", numero1);
+console.log("Numero 2 = ", numero2);
+
+let soma = numero1 + numero2;
+let subtracao = numero1 - numero2;
+let multiplicacao = numero1 * numero2;
+let divisao = numero1 / numero2;
+
+
+console.log("Soma = ", soma);
+console.log("Subtração = ", subtracao);
+console.log("Multiplicação = ", multiplicacao);
+console.log("Divisão = ", divisao);
---
+++ EX Basico/Exercicios Basico - 1/mensagem.js
@@ -0,0 +1,9 @@
+let nome = process.argv[2];
+let curso = process.argv[3];
+let mensagem = process.argv[4];
+
+
+console.log("Nome:", nome);
+console.log("Curso:", curso);
+console.log("Mensagem:", mensagem);
+
