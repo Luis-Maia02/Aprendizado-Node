200e65af05add183a66275c8e6f7943e71fee663
LUIS ENRIKE MAIA DA SILVA <luis-enrike_silva@estudante.sesisenai.org.br>
2026-07-02 20:53:28 -0300

    Exercicio 2 Modulo

---
+++ Modulo - Node/NIVEL - 1/Ex - 2/exibir.js
@@ -0,0 +1,7 @@
+const mensagens = require ("./mensagem");
+
+const nome = "Ze pilintra";
+
+mensagens.boasVindas(nome);
+mensagens.despedida(nome);
+mensagens.autorSistema();
---
+++ Modulo - Node/NIVEL - 1/Ex - 2/mensagem.js
@@ -0,0 +1,15 @@
+function boasVindas(nome) {
+    console.log("Opa", nome);
+}
+function despedida(nome) {
+    console.log("té logo", nome);
+}
+function autorSistema() {
+    console.log("Ze bobora");
+}
+
+module.exports = {
+    boasVindas,
+    despedida,
+    autorSistema
+}
