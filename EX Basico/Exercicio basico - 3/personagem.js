2bb32ad7bfbb1f0f7055edae7c8319acdd2019b1
LUIS ENRIKE MAIA DA SILVA <luis-enrike_silva@estudante.sesisenai.org.br>
2026-07-01 21:47:07 -0300

    Atividade node

---
+++ EX Basico/Exercicio basico - 3/personagem.js
@@ -0,0 +1,22 @@
+// Crie variáveis para:
+
+// - Nome
+// - Classe
+// - Nível
+// - Vida
+// - Mana
+
+let nome = "lucky luke";
+let classe = "Cowboy";
+let nivel = 99;
+let vida = 100;
+let mana = 420;
+
+console.log("Nome: ", nome);
+console.log("Classe: ", classe);
+console.log("Nível: ", nivel);
+console.log("Vida: ", vida);
+console.log("Mana: ", mana);
+
+
+
